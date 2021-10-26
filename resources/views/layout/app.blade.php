680ebd81a889c5a4407f9a8f870690d858a623b7
Michael Ogbuma <ogbumamichael@gmail.com>
2021-10-26 17:33:14 +0100

    Initial Commit

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Circle Link Health Dev Exercise</title>
+  <meta name="description" content="Sample test for laravel developer">
+  <meta name="author" content="Michel Ogbuma">
+</head>
+
+<body>
+  @yield('content')
+</body>
+</html>
